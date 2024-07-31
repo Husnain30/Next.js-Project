a26c0516c0477049b57e3d71212df56fe3d3912e
Muhammad Husnain <99895572+Husnain30@users.noreply.github.com>
2024-07-31 10:42:00 +0500

    website's Landing page is Fully Responsive now
    Landing page of our Website is created through next.js, tailwind CSS and Daisy UI. It is also fully Responsive now.

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -5,20 +5,20 @@ export default function Home() {
         <button className="btn btn-active btn-secondary sm:hidden md:block lg:block">Login</button>
       </div>
       <div className="flex flex-col justify-center items-center m-24 ">
-        <h1 className="font-sans font-bold text-4xl sm:text-3xl  ">
+        <h1 className="font-sans font-bold text-4xl   ">
           Website For Creating User Profile Template
         </h1>
-        <h1 className="py-6 font-light   ">
+        <h1 className="py-6 font-light md:m-2  ">
           Sign up for free and Create a profile of your own Interest
         </h1>
         <div>
         <input
         type="text"
         placeholder="Enter Your Email"
-        className="input input-bordered input-secondary pl-52 pr-32 mx-3 sm:px-12 bg-white my-9 "
+        className="input input-bordered input-secondary lg:pl-52 lg:pr-32 lg:mx-3 sm:px-40 bg-white lg:my-9 sm:my-7   "
       />
-      <button className="btn btn-active btn-secondary sm:mx-3 ">SignUp</button>
-      <button className="btn btn-active btn-secondary sm:block md:hidden lg:hidden">Login</button>
+      <button className="btn btn-active btn-secondary md:mx-2  ">SignUp</button>
+      <button className="btn btn-active btn-secondary sm:mx-2 md:hidden lg:hidden">Login</button>
       </div>
       </div>
      
